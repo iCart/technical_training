5f86c90996d9e564602875a06e5aa220ee6fa64d
Sylvain Francis <syf@odoo.com>
2020-06-30 11:37:17 +0200

    [ADD] awesome_tshirt : add dashboard buttons, remove counter widget

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -3,15 +3,42 @@ odoo.define('awesome_tshirt.dashboard', function (require) {
 
     var AbstractAction = require('web.AbstractAction');
     var core = require('web.core');
-    var Counter = require('awesome_tshirt.counter');
 
     var DashboardAction = AbstractAction.extend({
-        hasControlPanel: true,
-        start: function () {
-            var self = this;
-            return this._super.apply(arguments).then(function () {
-                var counter = new Counter(this, 4);
-                counter.appendTo(self.$('.o_content'));
+        template: 't_shirt_dashboard_template',
+        events: {
+            "click .customers_btn": 'show_customers',
+            "click .new_orders_btn": "show_new_orders",
+            "click .cancelled_orders_btn": "show_cancelled_orders",
+        },
+        show_customers: function () {
+            this.do_action({
+                res_model: 'res.partner',
+                name: 'View Customers',
+                views: [[false, 'kanban']],
+                type: 'ir.actions.act_window',
+            });
+        },
+        show_new_orders: function () {
+            var one_week_ago = new Date();
+            one_week_ago.setDate(one_week_ago.getDate() - 7);
+            this.do_action({
+                res_model: 'awesome_tshirt.order',
+                name: 'New Orders',
+                views: [[false, 'list']],
+                domain: [['create_date', '>', one_week_ago]],
+                type: 'ir.actions.act_window',
+            });
+        },
+        show_cancelled_orders: function () {
+            var one_week_ago = new Date();
+            one_week_ago.setDate(one_week_ago.getDate() - 7);
+            this.do_action({
+                res_model: 'awesome_tshirt.order',
+                name: 'Recent Cancelled Orders',
+                views: [[false, 'list']],
+                domain: [['create_date', '>', one_week_ago], ['state', '=', 'cancelled']],
+                type: 'ir.actions.act_window',
             });
         }
     });
